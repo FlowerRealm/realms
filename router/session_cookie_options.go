4980fef30897c3fdf58aba5c0fe6caa2eeb21188
FlowerRealm <flower_realm@outlook.com>
2026-03-12 18:01:38 +0800

    fix(review): address su8-reviewer round 4

--- router/session_cookie_options.go
+++ router/session_cookie_options.go
@@ -13,9 +13,6 @@ import (
 var trustedSessionCookieProxyCIDRs = []netip.Prefix{
 	netip.MustParsePrefix("127.0.0.1/32"),
 	netip.MustParsePrefix("::1/128"),
-	netip.MustParsePrefix("10.0.0.0/8"),
-	netip.MustParsePrefix("172.16.0.0/12"),
-	netip.MustParsePrefix("192.168.0.0/16"),
 }
 
 func applySessionCookieOptions(sess sessions.Session, r *http.Request) {
